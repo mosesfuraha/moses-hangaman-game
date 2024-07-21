d16c5529b11d65b91600748a5aeb5ba38bc145bf
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-07-21 20:16:24 +0200

    configured get by Category throuth the route

--- src/app/pages/singlecategory/singlecategory.component.spec.ts
+++ src/app/pages/singlecategory/singlecategory.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { SinglecategoryComponent } from './singlecategory.component';
+import { CategoryService } from '../../services/category.service';
+import { ModalService } from '../../services/modal.service';
+import { of } from 'rxjs';
 
 describe('SinglecategoryComponent', () => {
   let component: SinglecategoryComponent;
   let fixture: ComponentFixture<SinglecategoryComponent>;
 
   beforeEach(async () => {
+    const categorySerice = {
+      getCategories: () => of({ Sport: [{ selected: 'soccer' }] }),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [SinglecategoryComponent]
-    })
-    .compileComponents();
+      declarations: [SinglecategoryComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: (key: string) => (key === 'category' ? 'Sport' : null),
+              },
+            },
+          },
+        },
+        { provide: CategoryService, useValue: categorySerice },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SinglecategoryComponent);
     component = fixture.componentInstance;
